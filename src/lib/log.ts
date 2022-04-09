cda1e7fde9b3d89d5383d2065cf7f601fcd2c65d
Ryder Hicks <ryder@ryderhicks.com>
2022-04-09 16:41:05 -0600

    Env log channel

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -1,9 +1,10 @@
 import { TextChannel } from "discord.js";
 import { client } from "../bot";
+require("dotenv").config();
 
 export const LogEvent = (message: string) => {
   console.log(message);
-  const channel = client.channels.cache.get("962046090927038474") as TextChannel;
+  const channel = client.channels.cache.get(process.env.LOG_CHANNEL || "") as TextChannel;
   if (channel) {
     channel.send(message);
   }
