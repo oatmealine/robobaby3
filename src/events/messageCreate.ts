aaff3c1861cc149f2f141d8a6f616791240b91bd
Ryder Hicks <ryder@ryderhicks.com>
2022-04-09 14:31:09 -0600

    React to mentions of self

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,11 +1,19 @@
 import { Message } from "discord.js";
 import { removeInvites } from "../lib/message";
 
+const roboReacts: string[] = ["😉", "😍", "😘", "😜", "😝", "😎", "😏", "😒", "😓", "😔", "😖", "😞", "😣", "😢", "😭", "😨", "😩", "😫", "😬", "😰", "😱", "😲", "😳", "😴", "😵", "😶"];
+
 module.exports = {
   name: "messageCreate",
   once: false,
 
   async execute(message: Message) {
     removeInvites(message);
+
+    // react to name
+    await new Promise((resolve) => setTimeout(resolve, Math.random() * 4000 + 1000));
+    if (["robo", "baby"].some((el) => message.content.toLowerCase().includes(el))) {
+      message.react(roboReacts[Math.floor(Math.random() * roboReacts.length)]);
+    }
   },
 };
