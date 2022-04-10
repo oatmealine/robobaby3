ff1c7fe8564419e667736dec4f5ddd0a012cf0a9
Ryder Hicks <ryder@ryderhicks.com>
2022-04-09 20:51:53 -0600

    Cleverbot responses in spam channel

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,12 +1,14 @@
 import { Message } from "discord.js";
 import { reacts } from "../lib/reacts";
-import { removeInvites } from "../lib/message";
+import { removeInvites, respondToMessage } from "../lib/message";
 
 module.exports = {
   name: "messageCreate",
   once: false,
 
   async execute(message: Message) {
+    if (message.author.bot) return;
+
     removeInvites(message);
 
     // react to messages
@@ -15,5 +17,10 @@ module.exports = {
         message.react(reaction[Math.floor(Math.random() * reaction.length)]);
       }
     });
+
+    // respond if mentioned
+    if (message.channel.id == process.env.SPAM_CHANNEL && message.mentions.users.has(message.client.user?.id || "")) {
+      respondToMessage(message);
+    }
   },
 };
--- src/lib/message.ts
+++ src/lib/message.ts
@@ -1,5 +1,8 @@
-import { Message, Permissions } from "discord.js";
+import { Message, Permissions, Util } from "discord.js";
 import { LogEvent } from "./log";
+require("dotenv").config();
+
+const cleverbot = require("cleverbot-free");
 
 export const removeInvites = (message: Message) => {
   if (message.member?.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return;
@@ -9,3 +12,14 @@ export const removeInvites = (message: Message) => {
     LogEvent(`Invite link from ${message.author} deleted in ${message.channel}:\n${message.content}`);
   }
 };
+
+export async function respondToMessage(message: Message) {
+  await new Promise((r) => setTimeout(r, Math.random() * 5000 + 1000));
+  message.channel.sendTyping();
+  cleverbot(message.cleanContent).then((response: string) => {
+    response = response.toLowerCase();
+    if (response.endsWith(".")) response = response.slice(0, -1);
+
+    message.reply(response);
+  });
+}
