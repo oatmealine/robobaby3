d747a8d5f3ffd5654a8d26b968f86ca4ec93cb29
Ryder Hicks <ryder@ryderhicks.com>
2022-04-11 23:17:38 -0600

    Server move response

--- src/lib/responses.ts
+++ src/lib/responses.ts
@@ -1,27 +1,30 @@
 import { Message } from "discord.js";
+import { client } from "../bot";
 import { delay } from "./util";
+require("dotenv").config();
 
 interface PhraseResponder {
-  phrase: string;
+  phrases: Array<string>;
   response: string;
 }
 
 export const responses = [
-  { phrase: "crab dancing to chiptune", response: "https://youtu.be/j_d_4CnuqZQ" },
-  { phrase: "who is robo", response: "https://youtu.be/QY4AAos0daI" },
-  { phrase: "godmode chant", response: "It's a mod for pro 😁\nI love god\nI love godmode 😮\nIt's nice mode" },
+  { phrases: ["crab dancing to chiptune"], response: "https://youtu.be/j_d_4CnuqZQ" },
+  { phrases: ["who is robo"], response: "https://youtu.be/QY4AAos0daI" },
+  { phrases: ["godmode chant"], response: "It's a mod for pro 😁\nI love god\nI love godmode 😮\nIt's nice mode" },
+  { phrases: ["move server", "server move"], response: `There was no longer an active administrator account on the old server. Now that we have one, you can expect new **features**, **new moderators**, and **Robo-Baby 3.0**.` },
 ];
 
 export async function respondToMessage(message: Message): Promise<void> {
-  if (message.channel.id !== process.env.SPAM_CHANNEL) return;
-
   responses.forEach(async (pr: PhraseResponder) => {
-    if (message.content.includes(pr.phrase)) {
-      await delay(Math.random() * 2000 + 1000);
-      message.channel.sendTyping();
-      await delay(Math.random() * 750 + 750);
-      message.reply(pr.response);
-      return;
-    }
+    pr.phrases.forEach(async (phrase: string) => {
+      if (message.content.includes(phrase)) {
+        await delay(Math.random() * 1000 + 1000);
+        message.channel.sendTyping();
+        await delay(Math.random() * 750 + 750);
+        message.reply(pr.response);
+        return;
+      }
+    });
   });
 }
