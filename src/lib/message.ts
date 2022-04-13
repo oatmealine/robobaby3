69a1a39a3366208df2ee46dc554edf11d85ef340
Ryder Hicks <ryder@ryderhicks.com>
2022-04-13 15:23:33 -0600

    Better invite link detection

--- src/lib/message.ts
+++ src/lib/message.ts
@@ -1,11 +1,12 @@
 import { Message, Permissions } from "discord.js";
+import { getRandomEmoji } from "./util";
 require("dotenv").config();
 
 export const removeInvites = (message: Message): void => {
-  if (message.member?.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return;
+  //if (message.member?.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return;
 
-  if (message.content.includes("discord.gg")) {
+  if (message.content.match(/(https?:\/\/)?(www\.)?(discord\.(gg|io|me|li)|discordapp\.com\/invite)\/.+[a-z]/g)) {
     message.delete();
-    message.member?.send("Don't send invite links to other servers. If you must, send them to the interested users directly.");
+    message.member?.send(`Don't send invite links to other servers. If you must, send them to the interested parties directly. ${getRandomEmoji(message.guild)}`);
   }
 };
