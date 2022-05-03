483d150cc3b2d800cadfc6f21a2cb80982773604
Ryder Hicks <ryder@ryderhicks.com>
2022-05-03 14:08:18 -0600

    New reactions

--- src/lib/reactions.ts
+++ src/lib/reactions.ts
@@ -4,51 +4,26 @@ import { delay } from "./util";
 interface PhraseReactor {
   phrases: string[];
   reaction: string[];
-  chance: number;
+  chance?: number;
 }
 
-export const reacts = [
+export const reactions: Array<PhraseReactor> = [
   {
     phrases: ["robobaby", "robo-baby"],
-    reaction: [
-      "😉",
-      "😍",
-      "😘",
-      "😜",
-      "😝",
-      "😎",
-      "😏",
-      "😒",
-      "😓",
-      "😔",
-      "😖",
-      "😞",
-      "😣",
-      "😢",
-      "😭",
-      "😨",
-      "😩",
-      "😫",
-      "😬",
-      "😰",
-      "😱",
-      "😲",
-      "😳",
-      "😴",
-      "😵",
-      "😶",
-    ],
+    reaction: ["😉", "😍", "😘", "😜", "😝", "😎", "😏", "😒", "😓", "😔", "😖", "😞", "😣", "😢", "😭", "😨", "😩", "😫", "😬", "😰", "😱", "😲", "😳", "😶"],
     chance: 0.5,
   },
-  { phrases: ["butt"], reaction: ["🍑"], chance: 1 },
+  { phrases: ["butt"], reaction: ["🍑"] },
+  { phrases: ["crab"], reaction: ["🦀"] },
+  { phrases: ["should i", "am i", "can i"], reaction: ["👍", "👎"] },
 ];
 
 export const reactToMessage = async (message: Message): Promise<void> => {
   if (message.channel.id == process.env.CHANNEL_CHAT) return;
 
   await delay(Math.random() * 4000 + 1000);
-  reacts.forEach((pr: PhraseReactor) => {
-    if (Math.random() > pr.chance) return;
+  reactions.forEach((pr: PhraseReactor) => {
+    if (Math.random() > (pr.chance || 1)) return;
 
     if (pr.phrases.some((el) => ` ${message.cleanContent.toLowerCase()} `.includes(` ${el} `))) {
       message.react(pr.reaction[Math.floor(Math.random() * pr.reaction.length)]);
