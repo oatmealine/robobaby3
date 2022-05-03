b57b5e4d99398f215d034f02dce69315e64c6148
Ryder Hicks <ryder@ryderhicks.com>
2022-05-03 11:17:30 -0600

    Move redis bulk to ready event

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,8 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { Client, Collection } from "discord.js";
-import { SocketClosedUnexpectedlyError } from "redis";
-import { loadWatchlist } from "../lib/watchlist";
-import { redis } from "../lib/redis";
+import { connectToRedis } from "../lib/redis";
 import path from "path";
 import fs = require("node:fs");
 
@@ -21,24 +19,18 @@ module.exports = {
     });
 
     // load command functions
-    client.commands = new Collection();
-    const cmdFiles = fs.readdirSync(path.join(__dirname, "../commands")).filter((file: string) => file.endsWith(".js"));
-    for (const file of cmdFiles) {
-      const cmd = require(`../commands/${file}`);
-      client.commands.set(cmd.data.name, cmd);
-    }
+    loadCommands(client);
 
     // connect to db
-    redis
-      .connect()
-      .then(() => {
-        loadWatchlist();
-        console.log("Connected to redis database");
-      })
-      .catch(console.log);
-
-    redis.on("error", (err) => {
-      if (!(err instanceof SocketClosedUnexpectedlyError)) console.log("Redis error", err);
-    });
+    connectToRedis();
   },
+};
+
+const loadCommands = (client: Client) => {
+  client.commands = new Collection();
+  const cmdFiles = fs.readdirSync(path.join(__dirname, "../commands")).filter((file: string) => file.endsWith(".js"));
+  for (const file of cmdFiles) {
+    const cmd = require(`../commands/${file}`);
+    client.commands.set(cmd.data.name, cmd);
+  }
 };
--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,5 +1,20 @@
-import { createClient } from "redis";
+import { createClient, SocketClosedUnexpectedlyError } from "redis";
+import { loadWatchlist } from "./watchlist";
 import * as dotenv from "dotenv";
 dotenv.config();
 
 export const redis = createClient({ url: process.env.REDISTOGO_URL });
+
+export const connectToRedis = () => {
+  redis
+    .connect()
+    .then(() => {
+      loadWatchlist();
+      console.log("Connected to redis database");
+    })
+    .catch(console.log);
+
+  redis.on("error", (err) => {
+    if (!(err instanceof SocketClosedUnexpectedlyError)) console.log("Redis error", err);
+  });
+};
