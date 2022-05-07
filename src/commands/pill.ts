f6bb34f8fb8611c61917e667b3e792ba09736e0d
Ryder Hicks <ryder@ryderhicks.com>
2022-05-06 23:17:27 -0600

    12 hour cooldown

--- src/commands/pill.ts
+++ src/commands/pill.ts
@@ -8,43 +8,42 @@ dotenv.config();
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const pluralize = require("pluralize");
-const cooldown = 1000 * 60 * 60;
+const cooldown = 1000 * 60 * 60 * 12;
 
 module.exports = {
   data: new SlashCommandBuilder().setName("pill").setDescription("💊 Eat a random pill."),
 
   async execute(interaction: CommandInteraction, member: GuildMember) {
     // cooldown
-    // const timeKey = `pill:${member.id}`;
-    // if (await redis.exists(timeKey)) {
-    //   const lastUsed = parseInt((await redis.get(timeKey)) || "0");
-    //   const elapsed = interaction.createdTimestamp - lastUsed;
-    //   if (elapsed < cooldown) {
-    //     const seconds = Math.floor((cooldown - elapsed) / 1000);
-    //     const minutes = Math.floor(seconds / 60);
-    //     const left =
-    //       seconds > 60
-    //         ? `${minutes} ${pluralize("minute", minutes)} and ${seconds % 60} ${pluralize("second", seconds % 60)}`
-    //         : `${seconds} ${pluralize("second", seconds)}`;
+    const timeKey = `pill:${member.id}`;
+    if ((await redis.exists(timeKey)) && !member.roles.cache.find((r) => r.id == process.env.ROLE_MOD)) {
+      const lastUsed = parseInt((await redis.get(timeKey)) || "0");
+      const elapsed = interaction.createdTimestamp - lastUsed;
+      if (elapsed < cooldown) {
+        const minutes = Math.floor((cooldown - elapsed) / 1000 / 60);
+        const hours = Math.floor(minutes / 60);
+        const left =
+          hours > 1
+            ? `${hours} ${pluralize("hour", hours)} and ${minutes % 60} ${pluralize("minute", minutes % 60)}`
+            : `${minutes} ${pluralize("minute", minutes)}`;
 
-    //     interaction.reply({
-    //       content: `You can have another one in \`${left}\`.`,
-    //       ephemeral: true,
-    //     });
-    //     return;
-    //   }
-    // }
-    // redis.set(timeKey, interaction.createdTimestamp);
+        interaction.reply({
+          content: `You can have another one in \`${left}\`.`,
+          ephemeral: true,
+        });
+        return;
+      }
+    }
+    redis.set(timeKey, interaction.createdTimestamp);
 
     // eat pill
     const pill = pills[Math.floor(Math.random() * pills.length)];
-    const embed = new MessageEmbed().setTitle(pill.icon).setDescription(pill.name).setColor(botColor);
+    const embed = new MessageEmbed().setTitle(`${pill.icon} » ${pill.name}`).setColor(botColor);
     if (pill.effect) pill.effect(member);
     interaction.reply({
-      content: "You ate a pill.",
       embeds: [embed],
       ephemeral: interaction.channel?.id != process.env.CHANNEL_CHAT,
     });
-    console.log(`${member.user.tag} ate a ${pill} pill.`);
+    console.log(`${member.user.tag} ate a pill: ${pill.name}`);
   },
 };
