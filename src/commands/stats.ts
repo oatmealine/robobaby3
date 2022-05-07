2836ce41ff89f278ccf6ddafa61fe132263fb99a
Ryder Hicks <ryder@ryderhicks.com>
2022-05-07 11:18:12 -0600

    Can use /stats on other members

--- src/commands/stats.ts
+++ src/commands/stats.ts
@@ -3,10 +3,16 @@ import { CommandInteraction, GuildMember } from "discord.js";
 import { GetMemberStatsEmbed } from "../lib/memberStats";
 
 module.exports = {
-  data: new SlashCommandBuilder().setName("stats").setDescription("🏆 View your member stats."),
+  data: new SlashCommandBuilder()
+    .setName("stats")
+    .setDescription("🏆 View your member stats.")
+    .addUserOption((option) => option.setName("member").setDescription("Member to view the stats of (leave blank for self).")),
 
   async execute(interaction: CommandInteraction, member: GuildMember) {
-    const embed = await GetMemberStatsEmbed(member);
+    const user = interaction.options.getUser("member");
+    const target = interaction.guild?.members.cache.get(user?.id || "") || member;
+
+    const embed = await GetMemberStatsEmbed(target);
     interaction.reply({ embeds: [embed], ephemeral: true });
   },
 };
