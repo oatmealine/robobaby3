11efe246eeff61fc84189dce876662c82c17c465
Ryder Hicks <ryder@ryderhicks.com>
2022-05-09 10:46:03 -0600

    /warn message field is optional

--- src/commands/warn.ts
+++ src/commands/warn.ts
@@ -10,9 +10,9 @@ module.exports = {
     .setDescription("[MODERATOR] Apply a warning to a member.")
     .setDefaultPermission(false)
     .addUserOption((option) => option.setName("target").setDescription("The member to warn.").setRequired(true))
-    .addStringOption((option) => option.setName("message").setDescription("ID of the offending message.").setRequired(true))
     .addStringOption((option) => option.setName("reason").setDescription("The reason for the warning.").setRequired(true))
-    .addBooleanOption((option) => option.setName("punish").setDescription("Whether or not to apply the associated punishment.").setRequired(true)),
+    .addBooleanOption((option) => option.setName("punish").setDescription("Whether or not to apply the associated punishment.").setRequired(true))
+    .addStringOption((option) => option.setName("message").setDescription("(Optional) ID of the offending message.").setRequired(false)),
 
   async execute(interaction: CommandInteraction, member: GuildMember) {
     const user = interaction.options.getUser("target");
@@ -43,31 +43,6 @@ module.exports = {
       return;
     }
 
-    // validate offending message
-    if (messageIdData?.length != 2) {
-      interaction.reply({
-        content: "Invalid message ID format. Use the format `CHANNELID-MESSAGEID`.\nYou can obtain this by holding shift and clicking the **Copy ID** button.",
-        ephemeral: true,
-      });
-      return;
-    }
-    const offendingChannel = (await interaction.client.channels.fetch(messageIdData[0])) as TextChannel;
-    const offendingMessage = await offendingChannel?.messages.fetch(messageIdData[1]);
-    if (!offendingMessage) {
-      interaction.reply({ content: "Couldn't find the offending message in this channel.", ephemeral: true });
-      return;
-    }
-    if (offendingMessage.author.id != target.id) {
-      interaction.reply({ content: "The specified message does not belong to the user you are warning.", ephemeral: true });
-      return;
-    }
-
-    const messageEmbed = new MessageEmbed();
-    messageEmbed
-      .setAuthor({ name: `The offending message in #${offendingChannel.name}:`, iconURL: target.user.displayAvatarURL() })
-      .setDescription(`**${target.displayName}:** ${offendingMessage.content}\n\n[View message](${offendingMessage.url})`)
-      .setColor(target.displayColor);
-
     // apply punishments
     const warnings = parseInt((await redis.get(`warnings:${target.id}`)) || "0") + 1;
     const applyPunishment = interaction.options.getBoolean("punish");
@@ -97,10 +72,8 @@ module.exports = {
       })
       .setTitle(`Reason: \`${reason}\``)
       .setColor(botColor);
-    await offendingMessage.reply({ embeds: [warningEmbed] });
-    interaction.reply({ content: `Warned ${target}.`, ephemeral: true });
 
-    // alert target
+    // alert embed
     const alertEmbed = new MessageEmbed()
       .setAuthor({
         name: `${member.displayName} gave you a warning`,
@@ -109,10 +82,46 @@ module.exports = {
       .setTitle(`Reason: \`${reason}\``)
       .setDescription(`**Warning:** \`${warnings}/4\`\n\n${applyPunishment ? "A punishment has been applied. " : ""}Please read our rules carefully.`)
       .setColor(botColor);
-    target.send({ embeds: [alertEmbed, messageEmbed] }).catch(() => {
-      interaction.followUp({ content: "Couldn't DM the user.", ephemeral: true }).catch(console.log);
-    });
     redis.set(`warnings:${target.id}`, warnings);
+
+    // if message supplied
+    if (messageIdData) {
+      if (messageIdData.length != 2) {
+        interaction.reply({
+          content:
+            "Invalid message ID format. Use the format `CHANNELID-MESSAGEID`.\nYou can obtain this by holding shift and clicking the **Copy ID** button.",
+          ephemeral: true,
+        });
+        return;
+      }
+      const offendingChannel = (await interaction.client.channels.fetch(messageIdData[0])) as TextChannel;
+      const offendingMessage = await offendingChannel?.messages.fetch(messageIdData[1]);
+      if (!offendingMessage) {
+        interaction.reply({ content: "Couldn't find the offending message.", ephemeral: true });
+        return;
+      }
+      if (offendingMessage.author.id != target.id) {
+        interaction.reply({ content: "The specified message does not belong to the user you are warning.", ephemeral: true });
+        return;
+      }
+
+      const messageEmbed = new MessageEmbed();
+      messageEmbed
+        .setAuthor({ name: `The offending message in #${offendingChannel.name}:`, iconURL: target.user.displayAvatarURL() })
+        .setDescription(`**${target.displayName}:** ${offendingMessage.content}\n\n[View message](${offendingMessage.url})`)
+        .setColor(target.displayColor);
+
+      await offendingMessage.reply({ embeds: [warningEmbed] });
+      target.send({ embeds: [alertEmbed, messageEmbed] }).catch(() => {
+        interaction.followUp({ content: "Couldn't DM the user.", ephemeral: true }).catch(console.log);
+      });
+    } else {
+      // if no message
+      await interaction.reply({ embeds: [warningEmbed] });
+      target.send({ embeds: [alertEmbed] }).catch(() => {
+        interaction.followUp({ content: "Couldn't DM the user.", ephemeral: true }).catch(console.log);
+      });
+    }
 
     LogEvent(`${member} warned ${target} for \`${reason}\` (${warnings}/4)`);
   },
