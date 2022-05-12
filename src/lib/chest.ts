8b51b082a60d865a40e6470c61bba9fd4f64dead
Ryder Hicks <ryder@ryderhicks.com>
2022-05-12 11:20:26 -0600

    Better chest button display

--- src/lib/chest.ts
+++ src/lib/chest.ts
@@ -2,6 +2,7 @@ import { ButtonInteraction, Client, Message, MessageActionRow, MessageButton, Me
 import { chests } from "./data/chests";
 import { MemberStats } from "./data/stats";
 import { AdjustMemberStat, GetMemberStat, GetMemberStatsEmbed } from "./memberStats";
+import { botColor } from "./util";
 
 export const initializeChestGenerator = (client: Client) => {
   setInterval(() => {
@@ -58,14 +59,16 @@ const openChest = async (i: ButtonInteraction, chestType: string) => {
   // open chest
   const msg = i.message as Message;
   const row = new MessageActionRow().addComponents(
-    new MessageButton().setCustomId("nope").setLabel(`Claimed by ${member.displayName}`).setStyle("DANGER").setDisabled(true),
+    new MessageButton().setCustomId("nope").setLabel(`Claimed by ${member.displayName}`).setStyle("DANGER").setDisabled(true)
+  );
+  const statsRow = new MessageActionRow().addComponents(
     new MessageButton().setCustomId(member.id).setLabel(`View ${member.displayName}'s stats`).setStyle("SECONDARY")
   );
-  msg.edit({ files: [`./images/chests/${chestType}/open.png`], components: [row] }).catch(console.log);
+  msg.edit({ files: [`./images/chests/${chestType}/open.png`], components: [row, statsRow] }).catch(console.log);
 
   // get loot
   const loot = chests[chestType].possibleContents();
-  const embed = new MessageEmbed().setTitle("Contents");
+  const embed = new MessageEmbed().setTitle("Contents").setColor(botColor);
   for (const [key, value] of Object.entries(loot)) {
     if (value === 0) continue;
     const stat = MemberStats[key];
