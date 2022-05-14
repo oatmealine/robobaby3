186511d52702a1cb98cc1aa5aabbb78f9fbca2b2
Ryder Hicks <ryder@ryderhicks.com>
2022-05-14 13:07:12 -0600

    Log nickname removals

--- src/events/guildMemberUpdate.ts
+++ src/events/guildMemberUpdate.ts
@@ -7,6 +7,12 @@ module.exports = {
 
   async execute(oldMember: GuildMember, newMember: GuildMember) {
     if (oldMember.nickname !== newMember.nickname) {
+      if (!newMember.nickname) {
+        LogEvent(`${newMember.user.tag} removed their nickname`);
+        console.log(`${oldMember.user.tag} removed their nickname`);
+        return;
+      }
+
       LogEvent(`${oldMember} changed their nickname to ${newMember.nickname}`);
       console.log(`${oldMember.user.tag} changed their nickname to ${newMember.nickname}`);
     }
